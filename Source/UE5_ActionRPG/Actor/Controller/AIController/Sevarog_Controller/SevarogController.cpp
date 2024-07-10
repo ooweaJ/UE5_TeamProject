c1a34837496fedba8f595835e142d62d5c82b037
LumenShurima <166148864+LumenShurima@users.noreply.github.com>
2024-07-10 11:42:32 +0900

    Add Data
    Add Sevarog Asset & Blueprint type Boss Model

---
+++ Source/UE5_ActionRPG/Actor/Controller/AIController/Sevarog_Controller/SevarogController.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor/Controller/AIController/Sevarog_Controller/SevarogController.h"
+
+
+ASevarogController::ASevarogController()
+{
+	Sight->SightRadius = 1000.f;
+}
