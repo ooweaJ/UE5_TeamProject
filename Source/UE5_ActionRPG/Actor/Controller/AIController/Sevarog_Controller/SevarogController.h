ebd1bf8dc870feee27d7cb8713b0561418b27d1a
LumenShurima <166148864+LumenShurima@users.noreply.github.com>
2024-07-10 12:34:47 +0900

    Add Data (#2)
    Add Sevarog Asset & Blueprint type Boss Model

---
+++ Source/UE5_ActionRPG/Actor/Controller/AIController/Sevarog_Controller/SevarogController.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actor/Controller/AIController/BaseAIController.h"
+#include "SevarogController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_ACTIONRPG_API ASevarogController : public ABaseAIController
+{
+	GENERATED_BODY()
+	
+	ASevarogController();
+	
+
+
+	
+};
