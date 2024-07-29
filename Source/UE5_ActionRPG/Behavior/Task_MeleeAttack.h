69f6123534511cc895661d96e5f0bf17f0bb415d
ooweaJ <dbwidn2@naver.com>
2024-07-29 09:50:25 +0900

    기본작업 틀

---
+++ Source/UE5_ActionRPG/Behavior/Task_MeleeAttack.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "Task_MeleeAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_ACTIONRPG_API UTask_MeleeAttack : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
