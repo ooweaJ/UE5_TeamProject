b463b9ab00d2be87ef36ced8a04bb9d151f8b3dc
pleasure97 <conple21@gmail.com>
2024-07-30 15:39:36 +0900

    (1) build.cs에 UI 관련된 public dependency 추가 (2) 캐릭터 선택 UI 개선 (3) 캐릭터 선택 후 GameInstance가 캐릭터 정보에 대한 구조체 저장하도록 개선

--- Source/UE5_ActionRPG/ASGameInstance.h
+++ Source/UE5_ActionRPG/ASGameInstance.h
@@ -6,6 +6,27 @@
 #include "Interfaces/OnlineSessionInterface.h"
 #include "ASGameInstance.generated.h"
 
+
+UENUM(BlueprintType)
+enum class ECharacterClass : uint8
+{
+	Warrior UMETA(DisplayName = "Warrior"),
+	Ranger UMETA(DisplayName = "Ranger"),
+	Swordman UMETA(DisplayName = "Swordman"),
+	Spearman UMETA(DisplayName = "Spearman"),
+	_End UMETA(Hidden)
+};
+
+USTRUCT()
+struct FCharacterData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	ECharacterClass CharacterClassName;
+};
+
+
 UCLASS()
 class UE5_ACTIONRPG_API UASGameInstance : public UGameInstance
 {
@@ -14,6 +35,10 @@ class UE5_ACTIONRPG_API UASGameInstance : public UGameInstance
 public:
 	UASGameInstance(const FObjectInitializer& ObjectInitializer);
 	virtual void Init() override;
+
+public:
+	UPROPERTY(EditAnywhere)
+	FCharacterData CharacterData; 
 
 private:
 	void OnCreateSessionComplete(FName InSessionName, bool InSuccess);
