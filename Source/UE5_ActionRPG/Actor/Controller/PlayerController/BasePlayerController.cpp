319cb7bf79ba3258e938cd368c47681b96331edb
LumenShurima <166148864+LumenShurima@users.noreply.github.com>
2024-07-31 13:20:29 +0900

    Commit Testing LockOn System

--- Source/UE5_ActionRPG/Actor/Controller/PlayerController/BasePlayerController.cpp
+++ Source/UE5_ActionRPG/Actor/Controller/PlayerController/BasePlayerController.cpp
@@ -8,6 +8,7 @@
 #include "BaseGameplayTags.h"
 #include "KismetAnimationLibrary.h"
 #include "Kismet/KismetMathLibrary.h"
+#include "Camera/CameraComponent.h"
 
 ABasePlayerController::ABasePlayerController()
 {
@@ -60,50 +61,47 @@ void ABasePlayerController::OnMove(const FInputActionValue& InputActionValue)
 
 	const FRotator Rotation = GetControlRotation();
 	const FRotator YawRotation(0, Rotation.Yaw, 0);
-	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	const FVector ForwardDirection = FRotator(0, Player->Camera->GetComponentRotation().Yaw,0).Vector();
+	const FVector RightDirection = FRotator(0, Player->Camera->GetComponentRotation().Yaw+90, 0).Vector();
 
 	ControlledPawn->AddMovementInput(ForwardDirection, MovementVector.Y);
 	ControlledPawn->AddMovementInput(RightDirection, MovementVector.X);
-	//FVector2D MovementVector = InputActionValue.Get<FVector2D>();
-	//APawn* ControlledPawn = GetPawn();
 
-	//const FRotator Rotation = GetControlRotation();
-	//const FRotator YawRotation(0, Rotation.Yaw, 0);
-	//const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-	//const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
-	//float AngleDampingSpeed = 1;
+	float AngleDampingSpeed = 1;
 
-	//if (Player)
-	//{
-	//	if (Player->WalkingDirectionAngle < 45 && Player->WalkingDirectionAngle > -45)
-	//	{
-	//		AngleDampingSpeed = 1.0;
+	if (Player)
+	{
+		if (Player->WalkingDirectionAngle < 45 && Player->WalkingDirectionAngle > -45)
+		{
+			AngleDampingSpeed = 1.0;
 
-	//	}
-	//	else
-	//	{
-	//		AngleDampingSpeed = 0.75;
-	//	}
-	//	ControlledPawn->AddMovementInput(ForwardDirection, MovementVector.Y * AngleDampingSpeed);
-	//	ControlledPawn->AddMovementInput(RightDirection, MovementVector.X * AngleDampingSpeed);
+		}
+		else
+		{
+			AngleDampingSpeed = 0.75;
+		}
+		ControlledPawn->AddMovementInput(ForwardDirection, MovementVector.Y * AngleDampingSpeed);
+		ControlledPawn->AddMovementInput(RightDirection, MovementVector.X * AngleDampingSpeed);
 
-	//	Player->ForwardInput = ForwardDirection * MovementVector.Y;
-	//	Player->RightInput = RightDirection * MovementVector.X;
-	//	Player->MoveDirection = (Player->ForwardInput + Player->RightInput).GetSafeNormal();
-	//	Player->WalkingDirectionAngle = UKismetAnimationLibrary::CalculateDirection(Player->MoveDirection, Player->GetActorRotation());
-	//	if (!Player->bLockOn)
-	//	{
-	//		Player->SetActorRotation(UKismetMathLibrary::RLerp(Player->GetActorRotation(), Player->MoveDirection.Rotation(), GWorld->GetDeltaSeconds() * Player->CharacterRotationAlphaLinearValue, true));
-	//	}
-	//}
+		Player->ForwardInput = ForwardDirection * MovementVector.Y;
+		Player->RightInput = RightDirection * MovementVector.X;
+		Player->MoveDirection = (Player->ForwardInput + Player->RightInput).GetSafeNormal();
+		Player->WalkingDirectionAngle = UKismetAnimationLibrary::CalculateDirection(Player->MoveDirection, Player->GetActorRotation());
+		if (!Player->bLockOn)
+		{
+			Player->SetActorRotation(UKismetMathLibrary::RLerp(Player->GetActorRotation(), Player->MoveDirection.Rotation(), GWorld->GetDeltaSeconds() * Player->CharacterRotationAlphaLinearValue, true));
+		}
+	}
 }
 
 void ABasePlayerController::OnLookMouse(const FInputActionValue& InputActionValue)
 {
-	const FVector ActionValue = InputActionValue.Get<FVector>();
-	AddYawInput(ActionValue.X);
-	AddPitchInput(ActionValue.Y);
+	if (!Player->bLockOn)
+	{
+		const FVector ActionValue = InputActionValue.Get<FVector>();
+		AddYawInput(ActionValue.X);
+		AddPitchInput(ActionValue.Y);
+	}
 }
 
 void ABasePlayerController::OnJump(const FInputActionValue& InputActionValue)
