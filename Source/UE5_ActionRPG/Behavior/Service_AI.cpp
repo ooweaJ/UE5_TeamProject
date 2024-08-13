4e701b19e16beeccb2dbc29a12e0995e00ac8e2d
DESKTOP-COA1C72\AR student <dudwogkrrmq@gmail.com>
2024-08-13 11:09:16 +0900

    pull

---
+++ Source/UE5_ActionRPG/Behavior/Service_AI.cpp
@@ -0,0 +1,47 @@
+#include "Behavior/Service_AI.h"
+#include "Actor/Controller/AIController/BaseAIController.h"
+#include "Actor/Character/AI/AIBaseCharacter.h"
+#include "Component/BehaviorComponent.h"
+#include "Component/StateComponent.h"
+
+UService_AI::UService_AI()
+{
+	NodeName = "AI";
+}
+
+void UService_AI::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	ABaseAIController* controller = Cast<ABaseAIController>(OwnerComp.GetOwner());
+	if (!controller) return;
+
+	UBehaviorComponent* behavior = controller->GetComponentByClass<UBehaviorComponent>();
+	if (!behavior) return;
+
+	AAIBaseCharacter* aiPawn = Cast<AAIBaseCharacter>(controller->GetPawn());
+
+	ACharacter* Target = behavior->GetTarget();
+
+	if (Target == nullptr)
+	{
+		behavior->SetWaitMode();
+		return;
+	}
+	else
+	{
+		float distance = aiPawn->GetDistanceTo(Target);
+
+		if (controller->GetAttackRange() > distance)
+		{
+			behavior->SetMeleeMode();
+			return;
+		}
+		else
+		{
+			behavior->SetApproachMode();
+			return;
+		}
+
+	}
+}
